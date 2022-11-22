2ae5f3b0cdc2811b8583d1e7c2ca9f7f6e530000
5star-rep <111691547+5star-rep@users.noreply.github.com>
2022-11-22 09:44:03 +0100

    Update blockchainActions.js

--- src/redux/blockchain/blockchainActions.js
+++ src/redux/blockchain/blockchainActions.js
@@ -48,7 +48,7 @@ export const connect = () => {
       },
     });
     const CONFIG = await configResponse.json();
-    const { ethereum } = window;
+    const { ethereum } = app;
     const metamaskIsInstalled = ethereum && ethereum.isMetaMask;
     if (metamaskIsInstalled) {
       Web3EthContract.setProvider(ethereum);
