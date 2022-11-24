4bfaae322599e653fdc2f678b386082320d889da
5star-rep <111691547+5star-rep@users.noreply.github.com>
2022-11-24 07:56:04 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -152,7 +152,7 @@ function App() {
       .then((receipt) => {
         console.log(receipt);
         setFeedback(
-          `WOW, the ${CONFIG.NFT_NAME} is now yours! sign in to Territory to view it.`
+          `WOW, the ${data.Supply += 1} is now yours! sign in to Territory to view it.`
         );
         setClaimingNft(false);
         dispatch(fetchData(blockchain.account));
