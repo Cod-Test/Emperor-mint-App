ae3dbc37e192265c23d7e95b1bc5c86a789430bf
Cod-Test <120358960+Cod-Test@users.noreply.github.com>
2023-01-06 15:40:39 +0100

    Update dataActions.js

--- src/redux/data/dataActions.js
+++ src/redux/data/dataActions.js
@@ -25,9 +25,9 @@ export const fetchData = () => {
   return async (dispatch) => {
     dispatch(fetchDataRequest());
     try {
-      let Supply = await store
+      let _farmers = await store
         .getState()
-        .blockchain.smartContract.methods.Supply()
+        .blockchain.smartContract.methods._farmers()
         .call();
       // let cost = await store
       //   .getState()
@@ -36,7 +36,7 @@ export const fetchData = () => {
 
       dispatch(
         fetchDataSuccess({
-          Supply,
+          _farmers,
           // cost,
         })
       );
